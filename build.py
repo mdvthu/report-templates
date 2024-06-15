768e60dc6cbb0bff8f5ae05ae418764021a5e98c
Mark Thurston <mark@mdvthu.com>
2024-06-15 20:50:59 +0100

    Rewrite text templates obj orientated style
    For improved maintainability

--- build.py
+++ build.py
@@ -15,10 +15,13 @@ import jinja2
 
 import yaml
 
+# locations of user configuration files
 config_file = "config.yaml"
 config_file_template = "config.yaml.default"
-input_data_dir = "yaml"
-output_data_dir = "output/"
+
+cwd = os.path.abspath(os.path.dirname(__file__))
+INPUT_DATA_DIR = os.path.join(cwd, "yaml")
+OUTPUT_DATA_DIR = os.path.join(cwd, "output/")
 
 try:
     # load user configuration
@@ -41,48 +44,62 @@ if filecmp.cmp(config_file, config_file_template):
     )
     sys.exit(1)
 
-all_templates = {}
 
-# load the YAML templates into memory
-for fn in os.listdir(input_data_dir):
-    logging.debug("Processing %s", fn)
-    with open(os.path.join(input_data_dir, fn), "tr") as f:
-        yaml_content = yaml.safe_load(f)
-    logging.debug(yaml_content)
-    all_templates[fn] = yaml_content
+class YAMLTemplate:
+    def __init__(self, filename: str):
+        """
+        Load data and populate into a template
 
-# set up text templating
-env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
-# plain text report
-text_template = env.get_template("report.txt")
+        filename:
+            YAML file present within the template directory
+            (filename only, not full path)
+        """
+        self._yaml_path = os.path.join(INPUT_DATA_DIR, filename)
+        logging.debug("Loading template %s", filename)
+        with open(self._yaml_path, "tr") as f:
+            self.yaml_content = yaml.safe_load(f)
+        self.render_text()
 
-# create all the text templates
-for k in all_templates:
-    rendered_template = text_template.render(
-        config=config,
-        template_data=all_templates[k],
-    )
-    output_fn = "{}_{}.txt".format(
-        config["vr_shortcut_prefix"], all_templates[k]["vr_shortcut_suffix"]
-    ).replace(" ", "_")
-    # verbose output including the filename and rendered template
-    logging.info("output/text/%s:", output_fn)
-    logging.info("---")
-    logging.info(rendered_template)
-    logging.info("...")
-    # ensure the output directory exists
-    text_template_output_dir = os.path.join(output_data_dir, "text/")
-    os.makedirs(text_template_output_dir, exist_ok=True)
-    with open(os.path.join(text_template_output_dir, output_fn), "wt") as fout:
-        fout.write(rendered_template)
+    def __repr__(self):
+        return self.text_template
 
-# create a single XML file with all templates, for Dragon 12 import
-dragon_v12_template = env.get_template("dragon_mycmds.xml")
-rendered_dragon_template = dragon_v12_template.render(
-    config=config, template_data=all_templates
-)
-dragon_template_dir = os.path.join(output_data_dir, "dragon/")
-os.makedirs(dragon_template_dir, exist_ok=True)
-# Dragon VR templates XML
-with open(os.path.join(dragon_template_dir, "dragon_v12.xml"), "wt") as fout:
-    fout.write(rendered_dragon_template)
+    def render_text(self):
+        """Render plain text Jinja template"""
+        # set up text templating
+        env = jinja2.Environment(loader=jinja2.FileSystemLoader("templates"))
+        # plain text report
+        text_template = env.get_template("report.txt")
+        self.text_template = text_template.render(
+            config=config, template_data=self.yaml_content
+        )
+        # verbose output including the filename and rendered template
+        logging.info("---")
+        logging.info(self.text_template)
+        logging.info("...")
+
+    def save(self):
+        """Save the text template to disk"""
+        # ensure the output directory exists
+        text_template_loc = os.path.join(OUTPUT_DATA_DIR, "text/")
+        os.makedirs(text_template_loc, exist_ok=True)
+        output_fn = "{}.txt".format(
+            self.yaml_content["vr_shortcut_suffix"].replace(" ", "_")
+        )
+        with open(os.path.join(text_template_loc, output_fn), "wt") as fout:
+            fout.write(self.text_template)
+
+
+all_templates = {YAMLTemplate(fn) for fn in os.listdir(INPUT_DATA_DIR)}
+
+for template in all_templates:
+    template.save()
+
+# # create a single XML file with all templates, for Dragon 12 import
+# dragon_v12_template = env.get_template("dragon_mycmds.xml")
+# rendered_dragon_template = dragon_v12_template.render(
+#    config=config, template_data=all_templates)
+# dragon_template_dir = os.path.join(output_data_dir, "dragon/")
+# os.makedirs(dragon_template_dir, exist_ok=True)
+# # Dragon VR templates XML
+# with open(os.path.join(dragon_template_dir, "dragon_v12.xml"), "wt") as fout:
+#    fout.write(rendered_dragon_template)
